bb004f4e9292454448b03e32171b1cafc7021966
Elle <elle.a.gassner@gmail.com>
2022-03-27 16:03:48 -0700

    inital values set up for userData in comment Form

--- apps.js
+++ apps.js
@@ -4,8 +4,8 @@
 //have a conditional statment that says if the user clicks on dropdown
     //add eventlistener
 
-const parentMenu = document.querySelector('.dropdown');
-const subMenu = Array.from(parentMenu.querySelectorAll('li'));
+const parentMenu = document.querySelector(".dropdown");
+const subMenu = Array.from(parentMenu.querySelectorAll("li"));
 
 function toggleDropdown(){
     for (let i = 0; i < subMenu.length; i++) {
@@ -19,3 +19,33 @@ function toggleDropdown(){
 }};
 
 
+// get info from user
+    //querySelectors for the form to store that info
+    //access and store data
+    //code will go in an evemt listener
+
+// create form elements from the user
+
+//display to the page
+
+const formEl = document.querySelector(".commentForm");
+
+formEl.addEventListener("submit", function(event){
+    event.preventDefault();
+
+    
+    const userName = document.getElementById("userName").value;
+    const userMessage = document.getElementById("userMessage").value;
+    // let date = new Date();
+
+    
+
+
+
+    console.log(userName);
+    console.log(userMessage);
+    console.log(userInputsDisplays);
+    console.log(date);
+
+
+})
